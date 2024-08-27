49a593d7bd5023cdf73404159cb7cd79151c94ec
Owen Ribera <owenribera@gmail.com>
2024-08-27 20:34:37 +0000

    Created SocialMediaController API request handler stubs

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -3,6 +3,9 @@ package Controller;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import Service.AccountService;
+import Service.MessageService;
+
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
  * found in readme.md as well as the test cases. You should
@@ -29,5 +32,72 @@ public class SocialMediaController {
         context.json("sample text");
     }
 
+    /*
+     * TODO: The registration will be successful if and only if the username is not blank, the password is at 
+     *  least 4 characters long, and an Account with that username does not already exist. If all these conditions 
+     *  are met, the response body should contain a JSON of the Account, including its account_id. The response 
+     *  status should be 200 OK, which is the default. The new account should be persisted to the database. 
+     *  If the registration is not successful, the response status should be 400. (Client error)
+     */
+    public void accountRegisterHandler(String un, String pw) {}
 
+    /*
+     * TODO: The login will be successful if and only if the username and password provided in the request body 
+     *  JSON match a real account existing on the database. If successful, the response body should contain a 
+     *  JSON of the account in the response body, including its account_id. The response status should be 200 OK, 
+     *  which is the default.
+     *  If the login is not successful, the response status should be 401. (Unauthoriz
+    */
+    public void accountLoginHandler(Context ctx) {}
+
+    /*
+     * TODO: The creation of the message will be successful if and only if the message_text is not blank, is under 
+     *  255 characters, and posted_by refers to a real, existing user. If successful, the response body should 
+     *  contain a JSON of the message, including its message_id. The response status should be 200, which is the 
+     *  default. The new message should be persisted to the database. If the creation of the message is not 
+     * successful, the response status should be 400. (Client error)
+     */
+    public void createNewMessageHandler(Context ctx) {}
+
+    /*
+     * TODO: The response body should contain a JSON representation of a list containing all messages retrieved from 
+     *  the database. It is expected for the list to simply be empty if there are no messages. The response status 
+     *  should always be 200, which is the default.
+     */
+    public void getAllMessagesHanlder(Context ctx) {}
+
+    /*
+    * TODO: The response body should contain a JSON representation of the message identified by the message_id. 
+    *  It is expected for the response body to simply be empty if there is no such message. The response status 
+    *  should always be 200, which is the default.
+    */
+   public void getMessageByIdHandler(Context ctx) {}
+
+   /*
+     * TODO: The deletion of an existing message should remove an existing message from the database. If the message 
+     *  existed, the response body should contain the now-deleted message. The response status should be 200, which 
+     *  is the default.
+     * If the message did not exist, the response status should be 200, but the response body should be empty. This 
+     *  is because the DELETE verb is intended to be idempotent, ie, multiple calls to the DELETE endpoint should 
+     *  respond with the same type of response.
+     */
+    public void deleteMessageById(Context ctx) {}
+
+    /*
+     * TODO: The update of a message should be successful if and only if the message id already exists and the new 
+     *  message_text is not blank and is not over 255 characters. 
+     * If the update is successful, the response body should contain the full updated message (including 
+     *  message_id, posted_by, message_text, and time_posted_epoch), and the response status should be 200, which 
+     *  is the default. The message existing on the database should have the updated message_text.
+     * If the update of the message is not successful for any reason, the response status should be 400. (Client 
+     *  error)
+     */
+    public void updateMessageById(Context ctx) {}
+
+    /*
+     * TODO: The response body should contain a JSON representation of a list containing all messages posted by 
+     *  a particular user, which is retrieved from the database. It is expected for the list to simply be empty 
+     *  if there are no messages. The response status should always be 200, which is the default
+    */
+    public void getAllMessagesFromUserHandler(Context ctx) {}
 }
